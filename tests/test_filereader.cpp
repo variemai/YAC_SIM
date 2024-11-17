bc0af5689d4510335ceb692470252ea24c691e7a
Ioannis Vardas <vardas@par.tuwien.ac.at>
2024-11-18 00:08:09 +0100

    testing the filereader

--- tests/test_filereader.cpp
+++ tests/test_filereader.cpp
@@ -2,9 +2,17 @@
 #include <iostream>
 
 int main(int argc, char *argv[]){
+
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <trace_file_path>\n";
+        return 1;
+    }
+
+    std::string trace_file_path = argv[1];
+
     FileReader fr;
     std::vector<u32_t> trace;
-    fr.loadTrace("../inputs/ex11_5_trace.txt", trace);
+    fr.loadTrace(trace_file_path, trace);
     //print trace using iterator
     for(auto it = trace.begin(); it != trace.end(); it++){
         std::cout << *it << std::endl;
